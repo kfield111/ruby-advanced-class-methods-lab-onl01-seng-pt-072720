a22f62bf933e4a3c6adbbd010a5b0f1792d483f6
Kelin Field <kfield111@hotmail.com>
2020-08-18 02:24:11 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -47,7 +47,7 @@ def Song.alphabetical
 end
 
 def Song.new_from_filename (filename)
-  data = filename.delete(".mp3").split(" - ")
+  data = filename.delete(".mp3", "^m").split(" - ")
   puts data
   artist_name = data[0]
   name = data[1]
